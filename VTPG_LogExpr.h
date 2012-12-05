18d1cdad9e671266a31df3094ba0a8787cb1bcb5
Gil Shapira <gshapira@gmail.com>
2012-12-05 15:16:54 +0200

    Cleanup and add indirection to use third party logging

--- VTPG_LogExpr.h
+++ VTPG_LogExpr.h
@@ -1,34 +1,29 @@
-#pragma once
-
+//
 // Copyright (c) 2008-2010, Vincent Gable.
 // http://vincentgable.com
 //
 // Modified by MonsieurDart aka Mathieu Godart - L'atelier du mobile to extract only the LOG_EXPR code.
 //
 // Based off http://www.dribin.org/dave/blog/archives/2008/09/22/convert_to_nsstring/
+//
 
-NSString * VTPG_DDToStringFromTypeAndValue(const char * typeCode, void * value);
+#import <Foundation/Foundation.h>
 
-// WARNING: if NO_LOG_MACROS is #define-ed, than THE ARGUMENT WILL NOT BE EVALUATED
-#ifndef NO_LOG_MACROS
 
-#define LOG_EXPR(_X_) do{\
-	__typeof__(_X_) _Y_ = (_X_);\
-	const char * _TYPE_CODE_ = @encode(__typeof__(_X_));\
-	NSString *_STR_ = VTPG_DDToStringFromTypeAndValue(_TYPE_CODE_, &_Y_);\
-	if(_STR_)\
-		NSLog(@"%s = %@", #_X_, _STR_);\
-	else\
-		NSLog(@"Unknown _TYPE_CODE_: %s for expression %s in function %s, file %s, line %d", _TYPE_CODE_, #_X_, __func__, __FILE__, __LINE__);\
-}while(0)
+#define LOG_EXPR(EXPR)      LOG_EXPR_WITH(NSLog, EXPR)
 
-#define LOG_NS(...) NSLog(__VA_ARGS__)
-#define LOG_FUNCTION()	NSLog(@"%s", __func__)
 
-#else /* NO_LOG_MACROS */
+#define LOG_EXPR_WITH(LOGGER, EXPR)                                     \
+do {                                                                    \
+	__typeof__(EXPR) TYPE = (EXPR);                                     \
+	const char *TYPE_CODE = @encode(__typeof__(EXPR));                  \
+	NSString *MSG = VTPG_DDToStringFromTypeAndValue(TYPE_CODE, &TYPE);  \
+	if (MSG)                                                            \
+		LOGGER(@"%s = %@", #EXPR, MSG);                                 \
+	else                                                                \
+		LOGGER(@"Unknown TYPE_CODE: %s for expression %s",              \
+                TYPE_CODE, #EXPR);                                      \
+} while(0)
 
-#define LOG_EXPR(_X_)
-#define LOG_NS(...)
-#define LOG_FUNCTION()
 
-#endif /* NO_LOG_MACROS */
+extern NSString *VTPG_DDToStringFromTypeAndValue(const char *typeCode, void *value);
